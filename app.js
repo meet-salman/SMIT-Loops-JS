fbdffb00da6224f9a0300e84dd83002ab482099d
Salman Ahmed <say2salmanahmed@gmail.com>
2023-08-28 09:23:31 +0500

    Loops

---
+++ app.js
@@ -0,0 +1,80 @@
+const div = document.querySelector('div');
+
+// for (let i = 1; i <= 10; i++) {
+//     // console.log("6 * " + i + " = " + i*6);
+//     // console.log(`6 * ${i} = ${i * 6}`);
+//     div.innerHTML += (`6 * ${i} = ${i * 6} <br />`);
+// }
+
+
+// const numbers = [5, 6, 8, 3, 11, 23, 17, 27];
+// // let num1 = numbers.indexOf(11);
+// // console.log(num1);
+
+// if (numbers[4] == 11) {
+//     console.log("Number hy ga");
+
+// } else {
+//     console.log("Number nhi hy");
+// }
+
+
+const num = +prompt("Enter Number");
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     // div.innerHTML += (`${num} * ${i} = ${i * num} <br />`);
+//     div.innerHTML += (`<br />`);
+
+//     for (let j = 1; j <= 10; j++) {
+//         div.innerHTML += (`${i} * ${j} = ${i * j} <br />`);
+
+//     }
+
+// }
+
+
+
+
+
+
+
+
+if (num > 0) {
+    for (let i = 1; i <= 10; i++) {
+        div.innerHTML += (`${num} * ${i} = ${i * num} <br />`);
+    }
+
+} else {
+    alert("Invalid Number")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
